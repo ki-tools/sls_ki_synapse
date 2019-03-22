6d490662aad8f3cfc116210f03c54b740c3d5da1
hafen <rhafen@gmail.com>
2019-03-22 08:38:15 -0700

    Update slide deck generation

--- data/slide_deck/mutations/create_slide_deck.py
+++ data/slide_deck/mutations/create_slide_deck.py
@@ -136,23 +136,12 @@ class CreateSlideDeck(graphene.Mutation):
 
         CreateSlideDeck.move_to_front(presentation)
 
-        # problem statement
-
-        slide = presentation.slides.add_slide(plain_layout)
-        title_shape = CreateSlideDeck.get_placeholder(slide, 'Title 2')
-        body_shape = CreateSlideDeck.get_placeholder(slide, 'Text Placeholder 1')
-        title_shape.text = "Problem Statement"
-        body_shape.text = problem_statement
-        CreateSlideDeck.add_notes(slide)
-
-        CreateSlideDeck.move_to_front(presentation)
-
         # motivation
 
         slide = presentation.slides.add_slide(plain_layout)
         title_shape = CreateSlideDeck.get_placeholder(slide, 'Title 2')
         body_shape = CreateSlideDeck.get_placeholder(slide, 'Text Placeholder 1')
-        title_shape.text = "Motivation"
+        title_shape.text = "Value Hypothesis"
         body_shape.text = motivation
         CreateSlideDeck.add_notes(slide)
 
